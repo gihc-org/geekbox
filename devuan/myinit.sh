720ccbdec93efefc50aa4b883d9b18fdb775ba2e
Kristian Nygaard Jensen <kristian.n.jensen@gmail.com>
2026-08-14 11:32:03 +0200

    myinit: fb0-normalisering ved boot (bpp-flap mellem 16/32 pga. EDID-race)

--- devuan/myinit.sh
+++ devuan/myinit.sh
@@ -24,6 +24,18 @@ grep -q nameserver /etc/resolv.conf 2>/dev/null || \
 # -s: kun nøgle-login, ingen kodeord
 dropbear -s -R -p 22
 
+# fb0-normalisering: vendor-driveren rapporterer tilfældig bpp ift. den faktisk
+# allokerede buffer (EDID-race ved boot). Mål reel størrelse, tving var-info til
+# at matche, og vælg X-depth derefter (ellers dobbeltbillede/pixelrod).
+n=$(dd if=/dev/fb0 of=/dev/null bs=1M 2>&1 | grep -oE "^[0-9]+")
+if [ "${n:-0}" -ge 8000000 ]; then
+    fbset -fb /dev/fb0 -depth 32 -xres 1920 -yres 1080 2>/dev/null
+    sed -i "s/DefaultDepth .*/DefaultDepth 24/" /etc/X11/xorg.conf.d/fbdev.conf 2>/dev/null
+else
+    fbset -fb /dev/fb0 -depth 16 -xres 1920 -yres 1080 2>/dev/null
+    sed -i "s/DefaultDepth .*/DefaultDepth 16/" /etc/X11/xorg.conf.d/fbdev.conf 2>/dev/null
+fi
+
 {
   echo "=== myinit $(date) ==="
   echo "--- ip addr:"; ip addr
