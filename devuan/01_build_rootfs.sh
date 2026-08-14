9fb8298388a1dc5684de0d7c934af408e8e679a8
Kristian Nygaard Jensen <kristian.n.jensen@gmail.com>
2026-08-14 12:54:02 +0200

    01: dhclient-timeout, dansk locale/tidszone/tastatur, flere pakker (chrony, fbset, wireless-tools m.m.)

--- devuan/01_build_rootfs.sh
+++ devuan/01_build_rootfs.sh
@@ -55,9 +55,21 @@ grep -q ttyS2 "$ROOTFS/etc/inittab" || \
 echo "== ekstra pakker (ssh m.m.) =="
 chroot "$ROOTFS" /usr/bin/apt-get update
 chroot "$ROOTFS" /usr/bin/apt-get install -y --no-install-recommends \
-    openssh-server eudev wpasupplicant iputils-ping net-tools htop less vim-tiny
+    openssh-server eudev wpasupplicant iputils-ping net-tools htop less vim-tiny \
+    isc-dhcp-client wireless-tools locales chrony fbset
 # root-login via ssh til testbrug
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' "$ROOTFS/etc/ssh/sshd_config"
+
+# dhclient-timeout: et kabel-løst eth0 må ikke blokere wlan0 i ifupdown-rækken
+echo "timeout 15;" >> "$ROOTFS/etc/dhcp/dhclient.conf"
+
+# dansk locale + tastatur + tidszone
+sed -i "s/^# *da_DK.UTF-8 UTF-8/da_DK.UTF-8 UTF-8/" "$ROOTFS/etc/locale.gen"
+chroot "$ROOTFS" /usr/sbin/locale-gen
+chroot "$ROOTFS" /usr/sbin/update-locale LANG=da_DK.UTF-8
+ln -sf /usr/share/zoneinfo/Europe/Copenhagen "$ROOTFS/etc/localtime"
+echo "Europe/Copenhagen" > "$ROOTFS/etc/timezone"
+sed -i "s/^XKBLAYOUT=.*/XKBLAYOUT=\"dk\"/" "$ROOTFS/etc/default/keyboard" 2>/dev/null || true
 
 echo "== WiFi-firmware fra vendor =="
 mkdir -p "$ROOTFS/lib/firmware" "$ROOTFS/system/etc/firmware"
